01b1c11b953cc1c38dadb1838686c3a3055dcd92
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-05-06 09:49:38 +0530

    To reverse 4 digits number.

---
+++ Reverse of 4 digits number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	int num, temp;		
+	cout<<"ENTER THE 4 DIGIT NUMBER:- ";
+	cin>>num;		
+	cout<<"AFTER REVERSE RESULT IS:- ";
+{			
+	temp = num%10;
+	temp = temp%10;
+	cout<<temp;
+		
+	temp = num/10;
+	temp = temp%10;
+	cout<<temp;
+	
+	temp = num/10;
+	temp = temp/10;
+	cout<<temp;
+}
+	return 0;
+}
